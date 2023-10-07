432d2c220193ae03f6db38f3b7f08464584611ee
Vyacheslav Les <les.vyacheslav@gmail.com>
2023-10-07 17:56:24 +0300

    pitch carousel slides center

--- main.js
+++ main.js
@@ -2,7 +2,20 @@ function main() {
 
     $(".owl-carousel").owlCarousel({
         loop: true,
-        nav: true
+        nav: true,
+        center: true,
+        // autoWidth:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:2
+            }
+        }
     });
 
 }
